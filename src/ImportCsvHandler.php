95679cb5715bcffbf0703477787fd8bc94b8dfe5
Semenov <semenov@skeeks.com>
2020-02-06 12:57:52 +0300

    dev

--- src/ImportCsvHandler.php
+++ src/ImportCsvHandler.php
@@ -5,29 +5,28 @@
  * @copyright 2010 SkeekS (СкикС)
  * @date 29.08.2016
  */
+
 namespace skeeks\cms\importCsv;
 
-use skeeks\cms\base\ConfigFormInterface;
 use skeeks\cms\import\ImportHandler;
 use skeeks\cms\importCsv\models\ImportTaskCsv;
 use skeeks\cms\importCsv\widgets\ImportCsvWidget;
 use skeeks\cms\modules\admin\widgets\formInputs\OneImage;
-use yii\base\Model;
 use yii\helpers\FileHelper;
 use yii\helpers\Url;
 use yii\httpclient\Client;
 use yii\widgets\ActiveForm;
 
 /**
- * @property string $csvDelimetr
- * @property array $csvColumns
- * @property int $csvTotalRows
- * @property int $startRow
- * @property int $endRow
- * @property int $totalTask
- * @property int $totalSteps
+ * @property string  $csvDelimetr
+ * @property array   $csvColumns
+ * @property int     $csvTotalRows
+ * @property int     $startRow
+ * @property int     $endRow
+ * @property int     $totalTask
+ * @property int     $totalSteps
  *
- * @property string $rootFilePath
+ * @property string  $rootFilePath
  * @property boolean $isFileExists
  *
  * Class CsvHandler
@@ -36,7 +35,7 @@ use yii\widgets\ActiveForm;
  */
 abstract class ImportCsvHandler extends ImportHandler
 {
-    const CSV_TYPE_FIXED    = 'fixed';          //фиксированная ширина полей
+    const CSV_TYPE_FIXED = 'fixed';          //фиксированная ширина полей
     const CSV_TYPE_DELIMETR = 'delimetr';       //с разделителями - поля разделяются специальным символом
 
     const CSV_DELIMETR_TZP = 'TZP';                 //точка с запятой
@@ -45,8 +44,8 @@ abstract class ImportCsvHandler extends ImportHandler
     const CSV_DELIMETR_SPS = 'SPS';                 //пробел
     const CSV_DELIMETR_OTHER = 'OTHER';             //другой
 
-    const CSV_CHARSET_UTF8           = 'UTF-8';             //другой
-    const CSV_CHARSET_WINDOWS1251    = 'windows-1251';             //другой
+    const CSV_CHARSET_UTF8 = 'UTF-8';             //другой
+    const CSV_CHARSET_WINDOWS1251 = 'windows-1251';             //другой
 
     /**
      * @var string
@@ -78,10 +77,9 @@ abstract class ImportCsvHandler extends ImportHandler
      */
     public $csv_delimetr_other = ";";
 
-    public $csv_start_row   = 1;
-    public $csv_end_row     = '';
-    public $step            = 20;
-
+    public $csv_start_row = 1;
+    public $csv_end_row = '';
+    public $step = 20;
 
 
     public function getAvailableFields()
@@ -107,8 +105,8 @@ abstract class ImportCsvHandler extends ImportHandler
     static public function getCsvCharsets()
     {
         return [
-            self::CSV_CHARSET_UTF8 => self::CSV_CHARSET_UTF8,
-            self::CSV_CHARSET_WINDOWS1251    => self::CSV_CHARSET_WINDOWS1251,
+            self::CSV_CHARSET_UTF8        => self::CSV_CHARSET_UTF8,
+            self::CSV_CHARSET_WINDOWS1251 => self::CSV_CHARSET_WINDOWS1251,
         ];
     }
 
@@ -130,11 +128,11 @@ abstract class ImportCsvHandler extends ImportHandler
     static public function getCsvDelimetrTypes()
     {
         return [
-            self::CSV_DELIMETR_TZP          => 'Точка с запятой',
-            self::CSV_DELIMETR_ZPT          => 'Запятая',
-            self::CSV_DELIMETR_TAB          => 'Табуляция',
-            self::CSV_DELIMETR_SPS          => 'Пробел',
-            self::CSV_DELIMETR_OTHER        => 'Другой',
+            self::CSV_DELIMETR_TZP   => 'Точка с запятой',
+            self::CSV_DELIMETR_ZPT   => 'Запятая',
+            self::CSV_DELIMETR_TAB   => 'Табуляция',
+            self::CSV_DELIMETR_SPS   => 'Пробел',
+            self::CSV_DELIMETR_OTHER => 'Другой',
         ];
     }
 
@@ -144,20 +142,15 @@ abstract class ImportCsvHandler extends ImportHandler
     public function getCsvDelimetr()
     {
         $delimetr = ';';
-        if ($this->csv_delimetr_type == static::CSV_DELIMETR_TZP)
-        {
+        if ($this->csv_delimetr_type == static::CSV_DELIMETR_TZP) {
             $delimetr = ';';
-        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_ZPT)
-        {
+        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_ZPT) {
             $delimetr = ',';
-        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_TAB)
-        {
+        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_TAB) {
             $delimetr = '     ';
-        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_SPS)
-        {
+        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_SPS) {
             $delimetr = ' ';
-        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_OTHER)
-        {
+        } else if ($this->csv_delimetr_type == static::CSV_DELIMETR_OTHER) {
             $delimetr = $this->csv_delimetr_other;
         }
 
@@ -167,44 +160,47 @@ abstract class ImportCsvHandler extends ImportHandler
     public function rules()
     {
         return [
-            ['file_path' , 'required'],
-            ['file_path' , 'string'],
+            ['file_path', 'required'],
+            ['file_path', 'string'],
 
-            ['csv_type' , 'required'],
-            ['csv_type' , 'string'],
-            ['csv_type' , 'default', 'value' => self::CSV_TYPE_DELIMETR],
+            ['csv_type', 'required'],
+            ['csv_type', 'string'],
+            ['csv_type', 'default', 'value' => self::CSV_TYPE_DELIMETR],
 
-            ['csv_delimetr_type' , 'default', 'value' => self::CSV_DELIMETR_TZP],
-            ['csv_delimetr_type' , 'required'],
-            ['csv_delimetr_type' , 'string'],
+            ['csv_delimetr_type', 'default', 'value' => self::CSV_DELIMETR_TZP],
+            ['csv_delimetr_type', 'required'],
+            ['csv_delimetr_type', 'string'],
 
-            ['csv_source_charset' , 'string'],
+            ['csv_source_charset', 'string'],
 
-            ['csv_start_row' , 'integer'],
-            ['csv_end_row' , 'integer'],
+            ['csv_start_row', 'integer'],
+            ['csv_end_row', 'integer'],
 
-            ['step' , 'integer'],
-            ['step' , 'required'],
+            ['step', 'integer'],
+            ['step', 'required'],
 
-            ['csv_delimetr_other' , 'string'],
-            ['csv_delimetr_other' , 'required', 'when' => function(self $model)
-            {
-                return $model->csv_delimetr_type == self::CSV_DELIMETR_OTHER;
-            }],
+            ['csv_delimetr_other', 'string'],
+            [
+                'csv_delimetr_other',
+                'required',
+                'when' => function (self $model) {
+                    return $model->csv_delimetr_type == self::CSV_DELIMETR_OTHER;
+                },
+            ],
         ];
     }
 
     public function attributeLabels()
     {
         return [
-            'csv_type'                      => \Yii::t('skeeks/importCsv', 'CSV type'),
-            'csv_delimetr_type'             => \Yii::t('skeeks/importCsv', 'CSV type separator'),
-            'csv_delimetr_other'            => \Yii::t('skeeks/importCsv', 'Another separator'),
-            'csv_source_charset'            => \Yii::t('skeeks/importCsv', 'Encoding the source file'),
-            'csv_start_row'                 => \Yii::t('skeeks/importCsv', 'Start import from line'),
-            'csv_end_row'                   => \Yii::t('skeeks/importCsv', 'Finish the import on the line'),
-            'step'                          => \Yii::t('skeeks/importCsv', 'Import step'),
-            'file_path'                     => \Yii::t('skeeks/importCsv', 'The path to the file import CSV'),
+            'csv_type'           => \Yii::t('skeeks/importCsv', 'CSV type'),
+            'csv_delimetr_type'  => \Yii::t('skeeks/importCsv', 'CSV type separator'),
+            'csv_delimetr_other' => \Yii::t('skeeks/importCsv', 'Another separator'),
+            'csv_source_charset' => \Yii::t('skeeks/importCsv', 'Encoding the source file'),
+            'csv_start_row'      => \Yii::t('skeeks/importCsv', 'Start import from line'),
+            'csv_end_row'        => \Yii::t('skeeks/importCsv', 'Finish the import on the line'),
+            'step'               => \Yii::t('skeeks/importCsv', 'Import step'),
+            'file_path'          => \Yii::t('skeeks/importCsv', 'The path to the file import CSV'),
         ];
     }
 
@@ -220,15 +216,11 @@ abstract class ImportCsvHandler extends ImportHandler
         $counter = 0;
         $realCounter = $startRow;
 
-        while (($data = fgetcsv($handle, 0, $this->csvDelimetr)) !== FALSE)
-        {
-            if ($counter >= $startRow && $counter <= $endRow)
-            {
-                if (\Yii::$app->charset != $this->csv_source_charset)
-                {
+        while (($data = fgetcsv($handle, 0, $this->csvDelimetr)) !== false) {
+            if ($counter >= $startRow && $counter <= $endRow) {
+                if (\Yii::$app->charset != $this->csv_source_charset) {
                     $encodedData = [];
-                    foreach ($data as $row)
-                    {
+                    foreach ($data as $row) {
                         $row = iconv($this->csv_source_charset, \Yii::$app->charset, $row);
                         $encodedData[] = $row;
                     }
@@ -237,15 +229,14 @@ abstract class ImportCsvHandler extends ImportHandler
                 }
 
                 $result[$realCounter] = $data;
-                $realCounter ++;
+                $realCounter++;
             }
 
-            if ($counter > $endRow)
-            {
+            if ($counter > $endRow) {
                 break;
             }
 
-            $counter ++;
+            $counter++;
         }
 
         return $result;
@@ -260,8 +251,7 @@ abstract class ImportCsvHandler extends ImportHandler
 
         $handle = fopen($this->rootFilePath, "r");
 
-        while (($data = fgetcsv($handle, 0, $this->csvDelimetr)) !== FALSE)
-        {
+        while (($data = fgetcsv($handle, 0, $this->csvDelimetr)) !== false) {
             $counter++;
         }
 
@@ -273,9 +263,8 @@ abstract class ImportCsvHandler extends ImportHandler
      */
     public function getStartRow()
     {
-        if ($this->csv_start_row)
-        {
-            return (int) $this->csv_start_row;
+        if ($this->csv_start_row) {
+            return (int)$this->csv_start_row;
         }
 
         return 1;
@@ -286,9 +275,8 @@ abstract class ImportCsvHandler extends ImportHandler
      */
     public function getEndRow()
     {
-        if ($this->csv_end_row)
-        {
-            return (int) $this->csv_end_row;
+        if ($this->csv_end_row) {
+            return (int)$this->csv_end_row;
         }
 
         return $this->csvTotalRows;
@@ -307,7 +295,7 @@ abstract class ImportCsvHandler extends ImportHandler
      */
     public function getTotalSteps()
     {
-        return (int) round($this->totalTask / (int) $this->step) + 1;
+        return (int)round($this->totalTask / (int)$this->step) + 1;
     }
 
     protected $_root_file_path = null;
@@ -319,14 +307,14 @@ abstract class ImportCsvHandler extends ImportHandler
     {
         if ($this->_root_file_path === null) {
             if ($this->file_path && Url::isRelative($this->file_path)) {
-                $this->_root_file_path = \Yii::getAlias('@frontend/web' . $this->file_path);
+                $this->_root_file_path = \Yii::getAlias('@frontend/web'.$this->file_path);
             } else {
 
-                $tmpFile = \Yii::getAlias('@runtime/import/' . md5($this->file_path) . '.csv');
+                $tmpFile = \Yii::getAlias('@runtime/import/'.md5($this->file_path).'.csv');
                 //Если существует временный файл
                 if (file_exists($tmpFile)) {
                     //Если временный файл создан менее 5 минут назад, то можно использовать этот файл
-                    if (time() - filectime($tmpFile) <= 60*60*5 ) {
+                    if (time() - filectime($tmpFile) <= 60 * 60 * 5) {
                         $this->_root_file_path = $tmpFile;
                         return $tmpFile;
                     }
@@ -396,45 +384,41 @@ abstract class ImportCsvHandler extends ImportHandler
         echo $form->field($this, 'file_path')->widget(
             OneImage::className(),
             [
-                'showPreview'       => false,
-                'options' =>
-                [
-                    'data-form-reload'  => 'true'
-                ]
+                'showPreview' => false,
+                'options'     =>
+                    [
+                        'data-form-reload' => 'true',
+                    ],
             ]
         );
 
-        if ($this->file_path && !$this->isFileExists)
-        {
+        if ($this->file_path && !$this->isFileExists) {
             \yii\bootstrap\Alert::begin([
                 'options' => [
-                    'class' => 'alert-danger'
-                ]
+                    'class' => 'alert-danger',
+                ],
             ]);
-                echo \Yii::t('skeeks/import', 'Путь к файлу csv задан неверно или файл не существует по указанному пути');
+            echo \Yii::t('skeeks/import', 'Путь к файлу csv задан неверно или файл не существует по указанному пути');
             \yii\bootstrap\Alert::end();
-        } else
-        {
+        } else {
             echo $form->field($this, 'csv_type')->label(false)->radioList(static::getCsvTypes(), ['data-form-reload' => 'true']);
 
-            if ($this->csv_type == static::CSV_TYPE_DELIMETR)
-            {
+            if ($this->csv_type == static::CSV_TYPE_DELIMETR) {
                 echo $form->field($this, 'csv_delimetr_type')->label(false)->radioList(static::getCsvDelimetrTypes(), ['data-form-reload' => 'true']);
             }
 
             echo $form->field($this, 'csv_source_charset')->listBox(static::getCsvCharsets(), ['data-form-reload' => 'true', 'size' => 1]);
 
-            if ($this->csv_delimetr_type == static::CSV_DELIMETR_OTHER)
-            {
+            if ($this->csv_delimetr_type == static::CSV_DELIMETR_OTHER) {
                 echo $form->field($this, 'csv_delimetr_other')->textInput(['size' => 5]);
             }
 
             echo "<div class='row'><div class='col-md-3'>";
-                echo $form->field($this, 'csv_start_row');
+            echo $form->field($this, 'csv_start_row');
             echo "</div><div class='col-md-3'>";
-                echo $form->field($this, 'csv_end_row');
+            echo $form->field($this, 'csv_end_row');
             echo "</div><div class='col-md-3'>";
-                echo $form->field($this, 'step');
+            echo $form->field($this, 'step');
             echo "</div></div>";
         }
     }
@@ -445,11 +429,9 @@ abstract class ImportCsvHandler extends ImportHandler
     public function renderWidget(ActiveForm $form)
     {
         echo ImportCsvWidget::widget([
-            'activeForm' => $form
+            'activeForm' => $form,
         ]);
     }
-
-
 
 
 }
